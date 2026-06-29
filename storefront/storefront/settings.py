39578955c969cef0639784ae7c6a59bb218b3e01
tristan-spear <tspear1704@gmail.com>
2026-06-29 14:36:21 -0700

    finished yt tutorial

--- storefront/storefront/settings.py
+++ storefront/storefront/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     #'django.contrib.sessions', # Sessions is legacy, we do not use it in this project 
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'playgood', # Our app 
+    'playground', # Our app 
+    'debug_toolbar',
+    #'django.contrib.staticfiles',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'storefront.urls'
@@ -116,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+INTERNAL_IPS = ['127.0.0.1']
